1e67aa3a1ed8210bf5bfd80a9e8a5d5fbaba81db
abhishek sharma <imabhish143@gmail.com>
2025-12-20 21:10:42 +0530

    contact us

--- src/Components/Newsletter/Newsletter.tsx
+++ src/Components/Newsletter/Newsletter.tsx
@@ -1,47 +1,43 @@
 import { useInView } from 'framer-motion';
 import { useRef } from 'react';
 import './newsletter.css';
+
 export const Newsletter = () => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
+
   return (
     <section className="newsletter" ref={ref}>
       <div className="container">
         <div className="newsletter__wrapper">
-          <div className="newsletter__content">
-            <h2
-              style={{
-                transform: isInView ? 'none' : 'translateX(-100%)',
-                opacity: isInView ? 1 : 0,
-                transition: 'all 0.5s ease-out 0.2s',
-              }}
-            >
-              Be in the Know -
-              <span className="highlight">
-                {' '}
-                Subscribe to Our Newsletter!
-              </span>
-            </h2>
-            <form
-              className="newsletter__form"
-              style={{
-                transform: isInView ? 'none' : 'translateX(100%)',
-                opacity: isInView ? 1 : 0,
-                transition: 'all 0.5s ease-out 0.2s',
-              }}
-            >
-              <input
-                type="email"
-                className="email-input"
-                placeholder="Email..."
-              />
-              <input
-                type="submit"
-                className="primary__btn"
-                value="Subscribe!"
-              />
-            </form>
-          </div>
+          <h2
+            style={{
+              transform: isInView ? 'none' : 'translateY(-30px)',
+              opacity: isInView ? 1 : 0,
+              transition: 'all 0.5s ease-out',
+            }}
+          >
+            Be in the Know -
+            <span className="highlight"> Send Us a Message</span>
+          </h2>
+
+          <form
+            className="newsletter__form contact-form"
+            style={{
+              transform: isInView ? 'none' : 'translateY(30px)',
+              opacity: isInView ? 1 : 0,
+              transition: 'all 0.5s ease-out 0.2s',
+            }}
+          >
+            <input type="text" placeholder="Full Name" required />
+            <input type="email" placeholder="Email" required />
+            <input type="tel" placeholder="Phone Number" />
+            <textarea placeholder="Enter Your Message" rows={4}></textarea>
+
+            <button type="submit" className="primary__btn">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </section>
